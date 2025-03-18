fb259d3a1fe74a86e5fd4bf7eace286d32a7aa54
Nikunj <nikunj.singh@live.com>
2025-03-17 22:32:35 -0400

    centered main content div

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,14 @@
 function MainContent() {
-    return (
+  return (
+    <div className="mx-auto">
       <div>
         <h1 className="text-xl ">Main Content</h1>
-        <p>Here is the main content area that adjusts based on sidebar width.</p>
+        <p>
+          Here is the main content area that adjusts based on sidebar width.
+        </p>
       </div>
-    );
-  };
-  
-  export default MainContent;
+    </div>
+  );
+}
+
+export default MainContent;
